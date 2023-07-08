aea788536c3d78046e03f0c1fea4269f673ef49e
naruse666 <become15@i.softbank.jp>
2023-07-08 23:45:19 +0900

    feat: add role bind to aws-auth configmap

--- terraform/env/aws-auth.tf
+++ terraform/env/aws-auth.tf
@@ -12,12 +12,58 @@ resource "kubernetes_config_map" "aws_auth" {
   }
 
   data = {
-    "mapRoles" = <<EOT
-- rolearn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/*"
-  username: system:node:{{EC2PrivateDNSName}}
+    "mapUsers" = <<EOT
+- userarn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/cli-user"
+  username: admin
   groups:
     - system:bootstrappers
     - system:nodes
+    - eks-console-dashboard-full-access-binding"
+- userarn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/naruse"
+  username: admin
+  groups:
+    - system:bootstrappers
+    - system:nodes
+    - eks-console-dashboard-full-access-binding"
 EOT
+  }
+}
+
+# Role binding
+resource "kubernetes_role_binding" "aws-auth" {
+  metadata {
+    name      = "eks-console-dashboard-restricted-access-role-binding"
+    namespace = "default"
+  }
+
+  role_ref {
+    kind      = "Role"
+    api_group = "rbac.authorization.k8s.io"
+    name      = "eks-console-dashboard-restricted-access-role"
+  }
+
+  subject {
+    kind      = "Group"
+    api_group = "rbac.authorization.k8s.io"
+    name      = "eks-console-dashboard-restricted-access-group"
+  }
+}
+
+# Cluster Role Binding
+resource "kubernetes_cluster_role_binding" "aws-auth" {
+  metadata {
+    name = "eks-console-dashboard-full-access-binding"
+  }
+
+  subject {
+    kind      = "Group"
+    api_group = "rbac.authorization.k8s.io"
+    name      = "eks-console-dashboard-full-access-group"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    api_group = "rbac.authorization.k8s.io"
+    name      = "eks-console-dashboard-full-access-clusterrole"
   }
 }
