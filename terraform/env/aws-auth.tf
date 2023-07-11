4127eb957ea8303ce182f8f76ca1a5ed7bbf59c5
naruse <62323683+naruse666@users.noreply.github.com>
2023-07-11 14:51:01 +0900

    Deploy (#72)
    * fix: kms module error
    
    * fix: kms module, workflow authentication eks (#69)
    
    * fix: kms module, workflow authentication eks
    
    * fix: kms policy allow putKeyPolicy (#71)

--- terraform/env/aws-auth.tf
+++ terraform/env/aws-auth.tf
@@ -4,7 +4,9 @@ provider "kubernetes" {
   token                  = module.eks.cluster.token
 }
 
-resource "kubernetes_config_map" "aws_auth" {
+resource "kubernetes_config_map_v1_data" "aws_auth" {
+  force = true
+
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
