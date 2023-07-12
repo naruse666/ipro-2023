1aa789d4d36f72409ce20fe0f133febe3387e2f3
naruse666 <become15@i.softbank.jp>
2023-07-12 20:07:46 +0900

    fix: provider configuration

--- terraform/modules/eks/alb_controller.tf
+++ terraform/modules/eks/alb_controller.tf
@@ -1,14 +1,14 @@
 provider "helm" {
   kubernetes {
     host = aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = data.aws_eks_cluster_auth.cluster_auth.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
     token = data.aws_eks_cluster_auth.cluster_auth.token
   }
 }
 
 provider "kubernetes" {
     host = aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = data.aws_eks_cluster_auth.cluster_auth.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
     token = data.aws_eks_cluster_auth.cluster_auth.token
 }
 
