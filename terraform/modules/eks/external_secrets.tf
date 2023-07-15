a6b2d92d67e8fcab473a16cb370f87f7f96031c1
naruse <62323683+naruse666@users.noreply.github.com>
2023-07-15 18:13:22 +0900

    Feat/infra/eks/external secrets (#78)
    * feat: add external secrets

---
+++ terraform/modules/eks/external_secrets.tf
@@ -0,0 +1,106 @@
+data "aws_iam_policy_document" "oidc_external_secrets" {
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${aws_iam_openid_connect_provider.cluster.url}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${aws_iam_openid_connect_provider.cluster.url}:sub"
+      values   = ["system:serviceaccount:kube-system:external-secrets"]
+    }
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.cluster.arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "external_secrets" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:ListTagsForResource",
+      "ssm:DescribeParameters",
+    ]
+
+    resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+    ]
+
+    resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/*"]
+  }
+}
+
+resource "aws_iam_policy" "external_secrets" {
+  name   = "${var.cluster-name}-eks-external-secrets"
+  policy = data.aws_iam_policy_document.external_secrets.json
+}
+
+resource "aws_iam_role" "external_secrets" {
+  name               = "${var.cluster-name}-eks-external-secrets-role"
+  assume_role_policy = data.aws_iam_policy_document.oidc_external_secrets.json
+}
+
+resource "aws_iam_role_policy_attachment" "external_secrets" {
+  policy_arn = aws_iam_policy.external_secrets.arn
+  role       = aws_iam_role.external_secrets.name
+}
+
+resource "kubernetes_service_account" "external_secrets" {
+  metadata {
+    name      = "external_secrets"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.external_secrets.arn
+    }
+    labels = {
+      "app.kubernetes.io/component"  = "controller"
+      "app.kubernetes.io/name"       = "external_secrets"
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+}
+
+resource "helm_release" "external_secrets" {
+  name       = "external_secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets/external-secrets"
+  version    = "0.9.0"
+  namespace  = "kube-system"
+  depends_on = [
+    kubernetes_service_account.external_secrets,
+    null_resource.kubeconfig
+  ]
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = false
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-secrets"
+  }
+
+  set {
+    name  = "webhook.port"
+    value = 9443
+  }
+}
